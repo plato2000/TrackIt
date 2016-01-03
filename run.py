2a275de49fb368d2decd50eddbda7e671b3b1947
Parth Oza <plusparth@gmail.com>
2016-01-02 20:56:56 -0500

    removed Kevin's map testing stuff

--- run.py
+++ run.py
@@ -24,34 +24,11 @@ package_data = {}
 delivered_packages = []
 
 
-visited_points = [{'lat': begin[0], 'lng': begin[1]}]
-
-cp = begin[:]
-updates = 30
-dx = (end[0]-begin[0])/updates
-dy = (end[1]-begin[1])/updates
-
-
 def parse_time(time_str):
     """Does dateutil.parser.parse on the string, then converts to a \
     timetuple, which is converted to milliseconds since epoch by \
     time.mktime"""
     return int(time.mktime(parse(time_str).timetuple()))
-
-
-@app.route('/updateMap')
-def update():
-    global dx, dy, visited_points
-    # print(abs(cp[0] - end[0]))
-    if distance(cp, end) > 1:
-        cp[0] = cp[0]+dx
-        cp[1] = cp[1]+dy
-    x = cp[0]
-    y = cp[1]
-    if {'lat': cp[0], 'lng': cp[1]} not in visited_points:
-        visited_points += [{'lat': cp[0], 'lng': cp[1]}]
-    # print(visited_points)
-    return jsonify(a=x, b=y)
 
 
 @app.route('/reset')
@@ -139,8 +116,6 @@ def send_data():
     elif a == 'startingPoint':
         # print(jsonify(a=begin[0], b=begin[1]))
         return jsonify(a=begin[0], b=begin[1])
-    elif a == 'prevPoints':
-        return jsonify(results=visited_points)
     elif a == 'isDelivered':
         return jsonify(results=(uuid in delivered_packages))
     elif a == 'getNewPoints':
