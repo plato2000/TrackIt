f7bfc669b5a95ebbe4a6e95f9dcd955ffa5840fe
Parth Oza <plusparth@gmail.com>
2016-01-15 09:21:25 -0500

    Added python script to run event simulator in separate processes

---
+++ IDT_Server/package_events.py
@@ -0,0 +1,9 @@
+import subprocess
+import glob
+
+gpx_files = glob.glob("gpx_files/*.gpx")
+processes = [subprocess.Popen(["groovy", "PackageEventsSimulator.groovy",  "-n", n]) for n in gpx_files]
+print processes
+
+for p in processes:
+    p.wait()
